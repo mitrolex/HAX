f1736ccabcd094017fd5c993e3485517310f8c79
mitrolex <58146584+mitrolex@users.noreply.github.com>
2019-11-24 19:30:02 +0100

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,30 @@
+function PlaySound(CoxCarl) {
+    var thissound=document.getElementById(CoxCarl);
+    thissound.play();
+}
+
+function StopSound(CoxCarl) {
+    var thissound=document.getElementById(CoxCarl);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+    function PlaySound(Brejcha) {
+        var thissound=document.getElementById(Brejcha);
+        thissound.play();
+}
+
+    function StopSound(Brejcha) {
+        var thissound=document.getElementById(Brejcha);
+        thissound.pause();
+        thissound.currentTime = 0;
+}
+        function PlaySound(Djantrix) {
+            var thissound=document.getElementById(Djantrix);
+            thissound.play();
+}
+
+        function StopSound(Djantrix) {
+            var thissound=document.getElementById(Djantrix);
+            thissound.pause();
+            thissound.currentTime = 0;
+}
